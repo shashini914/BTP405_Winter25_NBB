2499c29bd1fd24370a348d9f335d2d1358674ea6
Shashini Ranasinghe <saranasinghe@myseneca.ca>
2025-02-05 17:21:39 -0500

    week05 lab02

---
+++ week05/lab02/client.py
@@ -0,0 +1,16 @@
+import socket
+import pickle
+
+# Data to send
+data = {"name": "Alice", "age": 24, "role": "developer"}
+
+# Create a TCP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 65433))
+
+# Serialize and send data
+pickled_data = pickle.dumps(data)
+client_socket.sendall(pickled_data)
+
+# Close connection
+client_socket.close()
